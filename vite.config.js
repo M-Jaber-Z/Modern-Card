b83514a89b11ac7040a37ad0308078a2415c3ae7
Moh_Jaber_Ziaee <ziaeejaber@gmail.com>
2026-07-03 02:11:20 -0700

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
   base:'/Modern-Card/',
-=======
-  base: 'Modern-Card',
->>>>>>> 2fe54c83b9ea5ff035b13d32c596f47e680b5dbf
   server: {
     port: 3000
   }
