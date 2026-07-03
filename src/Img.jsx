b1a9a17f9a7b56dcfda63d289e6f1fbfa9d37129
Moh_Jaber_Ziaee <ziaeejaber@gmail.com>
2026-07-03 03:19:28 -0700

    Update Img.jsx

--- src/Img.jsx
+++ src/Img.jsx
@@ -1,7 +1,7 @@
 const Image = () => {
     return (
         <div className="w-full flex justify-center items-center">
-            <img src="/assets/images/illustration-article.svg" className="w-10/10 mb-6 rounded-2xl" />
+            <img src="/assets/images/illustration.svg" className="w-10/10 mb-6 rounded-2xl" />
         </div>
     )
 }
