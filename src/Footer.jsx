6a8a9647260589d71817f7f34fd298c4124738ba
Moh_Jaber_Ziaee <ziaeejaber@gmail.com>
2026-07-03 02:52:21 -0700

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="flex justify-start items-center mt-4">
-            <img src="assets/images/pic.webp" className="w-10 h-10 rounded-full mr-4" />
+            <img src="/assets/images/pic.webp" className="w-10 h-10 rounded-full mr-4" />
             <h3 className="font-bold">Greg Hooper</h3>
         </div>
     )
